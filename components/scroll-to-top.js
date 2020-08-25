218a0b74477916194e800e8fae0358693a123ea9
fand <fand.gmork@gmail.com>
2020-08-25 11:17:14 +0900

    Peraichi

--- components/scroll-to-top.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  const scroll = () => {
-    if (typeof window === 'undefined' || window.scrollY === 0) { return; }
-    window.scrollTo(0, window.scrollY * 0.9);
-    requestAnimationFrame(scroll);
-  }
-
-  scroll();
-}
